7a033bbfcb7b3f7f300c682a58ff155cbf9f1190
Branden-git <brandenmobasso21@gmail.com>
2025-06-12 10:40:37 +0100

    Add files via upload
    avant la conversion en  php

---
+++ HTML de la page/Panier.php
@@ -0,0 +1,60 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Achat de biens</title>
+    <link rel="stylesheet" href="StyleSheet1.css">
+    <title>Panier</title>
+</head>
+<body class="panier">
+    <a href="Achat.html" class="link">Boutique</a>
+    <section>
+        <table>
+            <tr>
+                <th></th>
+                <th>Description</th>
+                <th>Ville</th>
+                <th>Prix</th>
+                <th>Quantité</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td>
+                    <img class="1" src="https://cf.bstatic.com/xdata/images/hotel/square600/695053286.webp?k=df7a9dfbb67a25ff0a459f0e36e149058f76c9ad92ee7efa364990cffd8599c6&o=">
+                </td>
+                <td>Appartement moderne</td>
+                <td>Yde</td>
+                <td>200000</td>
+                <td>5</td>
+                <td><img src="https://www.bing.com/th/id/OIP.-krCIPJCLQdvGv_oKDohcQHaHa?w=202&amp;h=211&amp;c=8&amp;rs=1&amp;qlt=90&amp;o=6&amp;dpr=1.3&amp;pid=3.1&amp;rm=2"></td>
+            </tr>
+            <tr>
+                <td>
+                    <img class="2" src="https://q-xx.bstatic.com/xdata/images/hotel/max500/406623913.jpg?k=9e52c86791b75f416b5dda5bb6ca97be04549f5ee85fff51dd7603cf6256e163&amp;o=" >
+                </td>
+                <td>Appartement de luxe</td>
+                <td>Yde</td>
+                <td>200000</td>
+                <td>5</td>
+                <td><img src="https://www.bing.com/th/id/OIP.-krCIPJCLQdvGv_oKDohcQHaHa?w=202&amp;h=211&amp;c=8&amp;rs=1&amp;qlt=90&amp;o=6&amp;dpr=1.3&amp;pid=3.1&amp;rm=2"></td>
+            </tr>
+            <tr>
+                <td>
+                    <img class="3" src="https://voyage.maresaonline.com/wp-content/uploads/2022/03/13-appartement-meubl%C3%A9-yaounde-tamtam.jpg.jpg?v=1648385085">
+                </td>
+                <td>Appartement simple et moderne</td>
+                <td>Yde</td>
+                <td>200000</td>
+                <td>5</td>
+                <td><img src="https://www.bing.com/th/id/OIP.-krCIPJCLQdvGv_oKDohcQHaHa?w=202&amp;h=211&amp;c=8&amp;rs=1&amp;qlt=90&amp;o=6&amp;dpr=1.3&amp;pid=3.1&amp;rm=2"></td>
+            </tr>
+            <tr class="total">
+                <th>Total : 1000000</th>
+            </tr>
+        </table>
+    </section>
+</body>
+</html>
